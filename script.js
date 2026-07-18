12c0cbaf2693276f348c85a2ec8ef8d7ba4e3baa
Tanaya Asalkar <145457627+tanayaasalkar1@users.noreply.github.com>
2026-07-18 19:21:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let itemCount = 1;
 
+// Function to add items
+function addItems(n) {
+  for (let i = 0; i < n; i++) {
+    const li = document.createElement("li");
+    li.innerText = `List Item ${itemCount++}`;
+    list.appendChild(li);
+  }
+}
+
+// Initial 10 items
+addItems(10);
+
+// Infinite scrolling
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight - 1) {
+    addItems(2);
+  }
+});
